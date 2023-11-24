7902764857b25c1118725b724fa843cc6b067a87
JooMin Choi <joominchoi23@gmail.com>
2023-11-24 15:08:23 +0900

    Add settings button to project notes view

--- src/pages/newProject.js
+++ src/pages/newProject.js
@@ -1,3 +1,6 @@
+import createProjectSettingsForm from "../components/projectSettingsForm";
+import openFormPopup from "../utils/openFormPopup";
+
 function createNewProjectNotesView(userInput) {
   const newProject = document.createElement("div");
   newProject.id = userInput;
@@ -6,7 +9,17 @@ function createNewProjectNotesView(userInput) {
   title.id = `${userInput}-title`;
   title.textContent = userInput
 
+  const settingsButton = document.createElement("button")
+  settingsButton.id = "settings-button"
+  settingsButton.textContent = "Settings"
+
+  settingsButton.addEventListener("click", (e) => {
+    console.log("Settings button has been clicked")
+    openFormPopup(createProjectSettingsForm());
+  });
+
   newProject.appendChild(title)
+  newProject.appendChild(settingsButton)
 
   return newProject;
 }
